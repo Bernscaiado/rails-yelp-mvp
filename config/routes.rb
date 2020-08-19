79fc1b5585e9f5a550a03b106941ec7a8e274a16
Bernardo Caiado <becaiado@gmail.com>
2020-08-19 10:53:44 -0700

    added basic functionalities

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :restaurants do
+
+  # get 'restaurants'
+
+  # get 'restaurants/new'
+  # post 'restaurants'
+
+  # get 'restaurants/member'
+
+  # get 'restaurants/member/reviews/new'
+  # post 'restaurants/member/reviews'
+
+  root to: 'restaurants#index'
+
+  resources :restaurants, only: [:index, :show, :new, :create] do
     resources :reviews, only: [:new, :create]
   end
 end
